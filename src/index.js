eed0360331402a8bb4f1e64458439e8708995190
Rafael Lacerda Silva <167188437+Taff4@users.noreply.github.com>
2025-07-23 15:54:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // Deve importar 'App', não 'SmartApp'
+import App from './App'; 
 import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

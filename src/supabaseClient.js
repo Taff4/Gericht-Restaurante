0f06be5987bd3fa996e47c83e973c5a3e16a2edf
Rafael Lacerda Silva <167188437+Taff4@users.noreply.github.com>
2025-07-23 15:54:41 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Seus dados de conexão com o Supabase
 const supabaseUrl = 'https://tfwigqcstiekdflgaryj.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmd2lncWNzdGlla2RmbGdhcnlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxMTkwNzEsImV4cCI6MjA2ODY5NTA3MX0.s_f8uF1TxHyOeipmxa1byw387xf5WgU72sqno4VAx4A';
 
-// Cria e exporta o cliente Supabase para ser usado em todo o projeto
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
